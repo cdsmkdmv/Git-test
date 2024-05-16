29b2a7812bf63cd4f24955e2d5eb4e33f27ff514
zx <zx@z.cn>
2024-05-16 15:47:08 +0800

    third commit

--- src/main/java/com/zx/gitDemo.java
+++ src/main/java/com/zx/gitDemo.java
@@ -3,5 +3,7 @@ package com.zx;
 public class gitDemo {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello git");
+        System.out.println("hello git 3");
     }
 }
