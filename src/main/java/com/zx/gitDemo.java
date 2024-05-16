561e59060363fac8c5b430b35b75172640bc8d62
zx <zx@z.cn>
2024-05-16 16:00:45 +0800

    hot-fix commit2

--- src/main/java/com/zx/gitDemo.java
+++ src/main/java/com/zx/gitDemo.java
@@ -6,6 +6,6 @@ public class gitDemo {
         System.out.println("hello git");
         System.out.println("hello git 3");
         System.out.println("hello1");
-        System.out.println("hello");
+        System.out.println("hotfix");
     }
 }
